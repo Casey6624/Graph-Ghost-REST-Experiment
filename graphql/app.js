cd0289855deec5119f2bc8e60a29b421d76a83d6
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-10-04 14:56:36 +0100

    Boilerplate created and setup with database connection and basic library imports. Now ready to begin the tests

---
+++ graphql/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = 4000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://dbadmin:Password123@experiment-yw6rd.mongodb.net/admin?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(4001);
+    console.log("Successfully Connected.");
+  })
+  .catch(err => {
+    console.log(`Ooops! Error: ${err}`);
+  });
+
+app.listen(PORT);
